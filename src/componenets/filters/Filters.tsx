e42827a4732dfcfe0f8a35f21c3ca4f80e41cd23
muradhaji <muradhajiyev42@icloud.com>
2025-08-09 20:56:03 +0400

    MonthlyStats component added

--- src/componenets/filters/Filters.tsx
+++ src/componenets/filters/Filters.tsx
@@ -4,7 +4,14 @@ import dayjs from 'dayjs';
 import { useTranslation } from 'react-i18next';
 import { useAppSelector } from '../../app/hooks';
 
-import { Button, SimpleGrid, Skeleton, Tooltip } from '@mantine/core';
+import {
+  Button,
+  Grid,
+  SimpleGrid,
+  Skeleton,
+  Stack,
+  Tooltip,
+} from '@mantine/core';
 import { Link } from 'react-router-dom';
 import { IconLibraryPlus } from '@tabler/icons-react';
 
@@ -25,13 +32,15 @@ import {
 import FilterCard from './FilterCard/FilterCard';
 import PageHeader from '../common/PageHeader/PageHeader';
 import StatsCard from './StatsCard/StatsCard';
+import MonthlyStats from './MonthlyStats/MonthlyStats';
+import type { MonthlyData, YearlyData } from '../../types/stats';
 
 const Filters = () => {
   const {
     installments,
     fetchInstallments: { loading: fetchInstallmentsLoading },
   } = useAppSelector((state) => state.installments);
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const { total, totalOverdue, totalCurrent, totalPaid, totalRemaining } =
     useMemo(() => {
@@ -74,6 +83,45 @@ const Filters = () => {
       };
     }, [installments]);
 
+  const monthlyStats: YearlyData[] = useMemo(() => {
+    const yearlyData: Record<number, Map<number, MonthlyData>> = {};
+
+    installments.forEach((installment) => {
+      installment.monthlyPayments.forEach((payment) => {
+        const date = dayjs(payment.date);
+        const year = date.year();
+        const index = date.month();
+        const monthLabel = date.format('MMM').toUpperCase();
+
+        if (!yearlyData[year]) yearlyData[year] = new Map();
+
+        const monthMap = yearlyData[year];
+        if (monthMap.has(index)) {
+          const existingMonth = monthMap.get(index)!;
+          existingMonth.amount = sumDecimal([
+            existingMonth.amount,
+            payment.amount,
+          ]);
+        } else {
+          monthMap.set(index, {
+            label: monthLabel,
+            amount: payment.amount,
+            index,
+          });
+        }
+      });
+    });
+
+    return Object.entries(yearlyData)
+      .map(([year, monthMap]) => ({
+        year: Number(year),
+        months: Array.from(monthMap.values()).sort((a, b) => a.index - b.index),
+      }))
+      .sort((a, b) => a.year - b.year);
+  }, [installments, i18n.language]);
+
+  console.log(monthlyStats);
+
   return (
     <>
       <PageHeader
@@ -94,58 +142,64 @@ const Filters = () => {
       />
 
       <Skeleton visible={fetchInstallmentsLoading}>
-        <SimpleGrid cols={{ base: 1, sm: 2 }} spacing='md'>
-          <StatsCard
-            total={total}
-            paid={totalPaid}
-            remaining={totalRemaining}
-          />
-
-          <SimpleGrid cols={{ base: 1, lg: 2 }} spacing='md'>
-            {totalOverdue !== 0 && (
-              <FilterCard
-                to='/payments/overdue'
-                label={t('components.filters.cards.overdue')}
-                stats={totalOverdue}
-                percentage={calculatePercentageDecimal(totalOverdue, total)}
-                color='red.5'
-                Icon={IconAlertSquareRounded}
+        <Grid gutter='md'>
+          <Grid.Col span={{ base: 12, sm: 6 }}>
+            <Stack>
+              <StatsCard
+                total={total}
+                paid={totalPaid}
+                remaining={totalRemaining}
               />
-            )}
-            <FilterCard
-              to='/payments/current'
-              label={t('components.filters.cards.current')}
-              stats={totalCurrent}
-              percentage={calculatePercentageDecimal(totalCurrent, total)}
-              color='orange'
-              Icon={IconCalendar}
-            />
-            <FilterCard
-              to='/payments/paid'
-              label={t('components.filters.cards.paid')}
-              stats={totalPaid}
-              percentage={calculatePercentageDecimal(totalPaid, total)}
-              color='teal.5'
-              Icon={IconSquareRoundedCheck}
-            />
-            <FilterCard
-              to='/payments/remaining'
-              label={t('components.filters.cards.remaining')}
-              stats={totalRemaining}
-              percentage={calculatePercentageDecimal(totalRemaining, total)}
-              color='gray.5'
-              Icon={IconCalendarClock}
-            />
-            <FilterCard
-              to='/payments/all'
-              label={t('components.filters.cards.all')}
-              stats={total}
-              percentage={100}
-              color='indigo.5'
-              Icon={IconLibrary}
-            />
-          </SimpleGrid>
-        </SimpleGrid>
+              <MonthlyStats data={monthlyStats} />
+            </Stack>
+          </Grid.Col>
+          <Grid.Col span={{ base: 12, sm: 6 }}>
+            <SimpleGrid cols={{ base: 1, lg: 2 }} spacing='md'>
+              {totalOverdue !== 0 && (
+                <FilterCard
+                  to='/payments/overdue'
+                  label={t('components.filters.cards.overdue')}
+                  stats={totalOverdue}
+                  percentage={calculatePercentageDecimal(totalOverdue, total)}
+                  color='red.5'
+                  Icon={IconAlertSquareRounded}
+                />
+              )}
+              <FilterCard
+                to='/payments/current'
+                label={t('components.filters.cards.current')}
+                stats={totalCurrent}
+                percentage={calculatePercentageDecimal(totalCurrent, total)}
+                color='orange'
+                Icon={IconCalendar}
+              />
+              <FilterCard
+                to='/payments/paid'
+                label={t('components.filters.cards.paid')}
+                stats={totalPaid}
+                percentage={calculatePercentageDecimal(totalPaid, total)}
+                color='teal.5'
+                Icon={IconSquareRoundedCheck}
+              />
+              <FilterCard
+                to='/payments/remaining'
+                label={t('components.filters.cards.remaining')}
+                stats={totalRemaining}
+                percentage={calculatePercentageDecimal(totalRemaining, total)}
+                color='gray.5'
+                Icon={IconCalendarClock}
+              />
+              <FilterCard
+                to='/payments/all'
+                label={t('components.filters.cards.all')}
+                stats={total}
+                percentage={100}
+                color='indigo.5'
+                Icon={IconLibrary}
+              />
+            </SimpleGrid>
+          </Grid.Col>
+        </Grid>
       </Skeleton>
     </>
   );
