9cab49c4d8705a965718525b4af2cb7c867cd842
muradhaji <muradhajiyev42@icloud.com>
2025-08-10 01:20:17 +0400

    translations changed

--- src/componenets/filters/FilterHeader/FilterHeader.tsx
+++ src/componenets/filters/FilterHeader/FilterHeader.tsx
@@ -1,7 +1,5 @@
 import { Card, Group, Text } from '@mantine/core';
 
-import utilStyles from '../../../styles/utils.module.css';
-
 const colorMap: Record<string, string> = {
   overdue: 'red.5',
   current: 'orange.5',
@@ -22,7 +20,7 @@ const FilterHeader = ({ title, amount, type }: FilterHeaderProps) => {
   return (
     <Card shadow='sm' radius='sm' withBorder mb='md' px='md' py='sm' bg='white'>
       <Group justify='space-between' gap='md' wrap='wrap'>
-        <Text size='lg' fw={700} c='gray.8' className={utilStyles.capitalize}>
+        <Text size='lg' fw={700} c='gray.8' tt='uppercase'>
           {title}
         </Text>
         <Text size='md' fw={700} c={color}>
